1433e080ce73afc40151f51802bb5e39c5dda195
AmandaPVSantos <169503404+AmandaPVSantos@users.noreply.github.com.>
2024-12-06 23:47:54 -0300

    incluindo banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner'
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          vamos aprender REACT! Finalmente agora vaaaaaaaaaaaaaai! ❤️😎🥳🥳🥳
+          REACT
         </p>
         <a
           className="App-link"
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+function Banner() {
+    // JSX = É como o react trabalha com a parte visual
+return (
+    <img src="/imagens/banner.png" alt="Banner principal"/>
+)
+}
+
+document.createElement('img')
+
+export default Banner
