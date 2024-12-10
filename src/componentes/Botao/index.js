0dedb015b4baebc0bf92397d54fef28dbebffcc1
AmandaPVSantos <169503404+AmandaPVSantos@users.noreply.github.com.>
2024-12-09 23:31:20 -0300

    Esilização do botao

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -2,10 +2,11 @@ import './Botao.css'
 
 const Botao = (props) => { // Função do botão
         // Função chamada quando o botão é clicado
-
-        return (<button>
-            {props.texto}
+        return (<button className='botao'>
+            
+            {props.children} 
         </button>)
+        // O {props.texto} será substituído por {props.children} <Tudo que estiver entre a abertura e fechamento do componente <botao> vai ser passado pela propriedade children>
 }
 
 export default Botao
