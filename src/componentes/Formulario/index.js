93b0e7a8e37aa683f8d63732ff4f6f299ccd8b75
AmandaPVSantos <169503404+AmandaPVSantos@users.noreply.github.com.>
2024-12-16 10:45:57 -0300

    incluido "onSubmit" no form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -14,9 +14,14 @@ const Formulario = () => {
         'Inovação e Gestão',
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()  // previne o comportamento padrão
+        console.log('Form foi submetido')
+    }
+
     return (
     <section className='formulario'>
-        <form>
+        <form onSubmit={aoSalvar}>
             <h2>Preencha os dados para criar o card do colaborador</h2>
             <CampoTexto label="Nome" placeholder="Digite seu nome"/>
             <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
